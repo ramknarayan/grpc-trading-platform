d3e126fc201dac0c6396ceaea3fd6eef9b39783f
ramanarayanakrishnappa <ramknarayan@gmail.com>
2024-08-17 13:49:54 -0400

    Full commit

--- aggregator-service/src/main/java/org/example/aggregator/controller/UserController.java
+++ aggregator-service/src/main/java/org/example/aggregator/controller/UserController.java
@@ -4,10 +4,7 @@ import org.example.aggregator.service.UserService;
 import org.example.user.UserInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -16,8 +13,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "{userId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserInformation getUserInformation(@PathVariable Integer userId){
+    @GetMapping(value="",produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserInformation getUserInformation(@RequestParam("userId") Integer userId){
         return this.userService.getUserInformation(userId);
     }
 
---
+++ aggregator-service/src/test/java/org/example/aggregator/tests/mockservice/StockMockService.java
@@ -0,0 +1,36 @@
+package org.example.aggregator.tests.mockservice;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import org.example.common.Ticker;
+import org.example.stock.PriceUpdate;
+import org.example.stock.StockPriceRequest;
+import org.example.stock.StockPriceResponse;
+import org.example.stock.StockServiceGrpc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class StockMockService extends StockServiceGrpc.StockServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(StockMockService.class);
+
+    @Override
+    public void getStockPrice(StockPriceRequest request, StreamObserver<StockPriceResponse> responseObserver) {
+        var response = StockPriceResponse.newBuilder().setPrice(15).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getPriceUpdates(Empty request, StreamObserver<PriceUpdate> responseObserver) {
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        for (int i = 1; i <= 5; i++) {
+            var priceUpdate = PriceUpdate.newBuilder().setPrice(i).setTicker(Ticker.AMAZON).build();
+            log.info("{}", priceUpdate);
+            responseObserver.onNext(priceUpdate);
+        }
+        responseObserver.onCompleted();
+    }
+}
